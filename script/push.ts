61791c15aa7d4155da8ef2170314ad240da77285
zxtreme <zxtreme@qq.com>
2023-09-07 15:32:51 +0800

    将drawboard优化为signature组件

--- script/push.ts
+++ script/push.ts
@@ -2,10 +2,12 @@ import run from './utils/run'
 import { series } from 'gulp'
 import { rootPath } from './utils/path'
 
+// 使用方式：npm run push 
 const project = "zview-ui";
 const remoteGH = "https://github.com/ZXtreme/zview-ui.git";
 const branch = "main";
 const description = process.argv.slice(2).find(arg => arg.startsWith('--description='))?.split('=')[1] ?? `deploy ${project} pages`;
+console.log(description);
 
 
 // 将所有文件推送到 GitHub
